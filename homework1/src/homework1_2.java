d77864db334ae38525f0f2cfa15b1bd6e330bf38
Sky Walker <yaroslav292@gmail.com>
2025-02-26 01:35:16 +0600

    init commit

---
+++ homework1/src/homework1_2.java
@@ -0,0 +1,51 @@
+public class homework1_2 {
+    public static void main(String[] args) {
+
+        int number = 100;
+        System.out.println(number);
+        double b = 1.543;
+        System.out.println(b);
+        boolean r = true;
+        System.out.println(r);
+        char e = 'L';
+        System.out.println(e);
+        char c1 = 'D';
+        System.out.println(c1);
+        char c2 = 40;
+        System.out.println(c2);
+        int n1 = 120;
+        int n2 = 90;
+        int n = n1 + n2;
+        System.out.println(n);
+        int n3 = n1 - n2;
+        System.out.println(n3);
+        double n11 = 90;
+        double n4 = n1 / n11;
+        System.out.println(n4);
+        int n5 = n1 * n2;
+        System.out.println(n5);
+        double n6 = ((n11 + 5) * 100) / n1;
+        System.out.println(n6);
+        int v = 30;
+        boolean v1 = v == 30;
+        System.out.println(v1);
+        boolean v2 = v != 30;
+        System.out.println(v2);
+        boolean v3 = v > 20;
+        System.out.println(v3);
+        int v11 = 20;
+        System.out.println(v > v11);
+        boolean v4 = v > 20 && v == 40;
+        System.out.println(v4);
+        boolean v5 = v != 30 || v >20;
+        System.out.println(v5);
+        System.out.println(-n1);
+        n1++;
+        System.out.println(n1);
+        n1--;
+        n1--;
+        n1--;
+        System.out.println(n1);
+        System.out.println(!v5);
+    }
+}
---
+++ homework1/src/homework1_2_1.java
@@ -0,0 +1,24 @@
+public class homework1_2_1 {
+    public static void main(String[] args) {
+
+    int number;
+    number = 100;
+    double b;
+    b = 1.573;
+    boolean r;
+    r = false;
+    char e;
+    e = 'S';
+    char c1;
+    char c2;
+    c1 = 'A';
+    c2 = 85;
+    System.out.println(c1);
+    System.out.println(c2);
+    System.out.println(number);
+    System.out.println(b);
+    System.out.println(r);
+    System.out.println(e);
+    }
+}
+
